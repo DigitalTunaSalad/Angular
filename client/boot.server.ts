510984e99bbd84d0a4b4c7b94add4517dbcaa2e1
Rasmus <rsj-kc@outlook.com>
2018-02-06 12:40:22 +0100

    added es6-promise

--- client/boot.server.ts
+++ client/boot.server.ts
@@ -15,48 +15,48 @@ import {
     PlatformState,
     INITIAL_CONFIG
 } from "@angular/platform-server";
-import {
-    createServerRenderer,
-    RenderResult
-} from "aspnet-prerendering";
+// import {
+//     createServerRenderer,
+//     RenderResult
+// } from "aspnet-prerendering";
 import { AppModule } from "./app/app.server.module";
 
 enableProdMode();
-export default createServerRenderer((params: any) => {
-    const providers: StaticProvider[] = [
-        {
-            provide: INITIAL_CONFIG,
-            useValue: {
-                document: "<app></app>",
-                url: params.url
-            }
-        },
-        {
-            provide: APP_BASE_HREF,
-            useValue: params.baseUrl
-        },
-        {
-            provide: "BASE_URL",
-            useValue: params.origin + params.baseUrl
-        }
-    ];
-    return platformDynamicServer(providers).bootstrapModule(AppModule).then((moduleRef: NgModuleRef<AppModule>) => {
-        const appRef: ApplicationRef = moduleRef.injector.get(ApplicationRef);
-        const state: PlatformState = moduleRef.injector.get(PlatformState);
-        const zone: NgZone = moduleRef.injector.get(NgZone);
+// export default createServerRenderer((params: any) => {
+//     const providers: StaticProvider[] = [
+//         {
+//             provide: INITIAL_CONFIG,
+//             useValue: {
+//                 document: "<app></app>",
+//                 url: params.url
+//             }
+//         },
+//         {
+//             provide: APP_BASE_HREF,
+//             useValue: params.baseUrl
+//         },
+//         {
+//             provide: "BASE_URL",
+//             useValue: params.origin + params.baseUrl
+//         }
+//     ];
+//     return platformDynamicServer(providers).bootstrapModule(AppModule).then((moduleRef: NgModuleRef<AppModule>) => {
+//         const appRef: ApplicationRef = moduleRef.injector.get(ApplicationRef);
+//         const state: PlatformState = moduleRef.injector.get(PlatformState);
+//         const zone: NgZone = moduleRef.injector.get(NgZone);
 
-        return new Promise<RenderResult>((resolve, reject) => {
-            zone.onError.subscribe((errorInfo: any) => reject(errorInfo));
-            appRef.isStable.first((isStable: boolean) => isStable).subscribe(() => {
-                // because 'onStable' fires before 'onError', we have to delay slightly before
-                // completing the request in case there's an error to report
-                setImmediate(() => {
-                    resolve({
-                        html: state.renderToString()
-                    });
-                    moduleRef.destroy();
-                });
-            });
-        });
-    });
-});
+//         return new Promise<RenderResult>((resolve, reject) => {
+//             zone.onError.subscribe((errorInfo: any) => reject(errorInfo));
+//             appRef.isStable.first((isStable: boolean) => isStable).subscribe(() => {
+//                 // because 'onStable' fires before 'onError', we have to delay slightly before
+//                 // completing the request in case there's an error to report
+//                 setImmediate(() => {
+//                     resolve({
+//                         html: state.renderToString()
+//                     });
+//                     moduleRef.destroy();
+//                 });
+//             });
+//         });
+//     });
+// });
