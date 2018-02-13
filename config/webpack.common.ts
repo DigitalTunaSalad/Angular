7a8026c5119beb814751fa666b3740e7b8504230
Rasmus <rsj-kc@outlook.com>
2018-02-13 08:22:34 +0100

    began to convert webpack to typescript

---
+++ config/webpack.common.ts
@@ -0,0 +1,72 @@
+import { Configuration, ContextReplacementPlugin } from "webpack";
+import * as ExtractTextPlugin from "extract-text-webpack-plugin";
+import { CheckerPlugin } from "awesome-typescript-loader";
+import * as helper from "./helper";
+export function common(env: any): Configuration {
+    const isDevBuild: boolean = !(env && env.prod);
+    const buildSass: boolean = env && env.sass;
+    const extractTextPlugin: ExtractTextPlugin = new ExtractTextPlugin({
+        filename: "theme.css",
+        disable: isDevBuild || buildSass
+    });
+    return {
+        stats: {
+            modules: false
+        },
+        context: __dirname,
+        resolve: {
+            extensions: [".js", ".ts"]
+        },
+        output: {
+            filename: "[name].js",
+            publicPath: "dist/"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: isDevBuild ? [
+                        "awesome-typescript-loader?silent=true",
+                        "angular2-template-loader",
+                        "angular2-router-loader"
+                    ] : "@ngtools/webpack"
+                },
+                {
+                    test: /\.html$/,
+                    use: "html-loader?minimize=false"
+                },
+                {
+                    test: /\.scss$/,
+                    include: helper.root("client", "app"),
+                    loaders: ["raw-loader", "sass-loader"]
+                },
+                {
+                    test: /\.scss$/,
+                    exclude: helper.root("client", "app"),
+                    include: helper.root("client", "theming"),
+                    use: extractTextPlugin.extract({
+                        use: [
+                            {
+                                loader: "css-loader"
+                            },
+                            {
+                                loader: "sass-loader"
+                            }
+                        ],
+                        // use style-loader in development
+                        fallback: "style-loader"
+                    })
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg)$/,
+                    use: "url-loader?limit=25000"
+                }
+            ]
+        },
+        plugins: [
+            new CheckerPlugin(),
+            new ContextReplacementPlugin(/angular(\\|\/)core/, helper.root("client")),
+            extractTextPlugin
+        ]
+    };
+}
---
+++ config/webpack.server.ts
@@ -0,0 +1,41 @@
+import { Configuration, Plugin, DllReferencePlugin } from "webpack";
+import * as merge from "webpack-merge";
+import { common } from "./webpack.common";
+import * as helper from "./helper";
+import { AngularCompilerPlugin } from "@ngtools/webpack";
+import * as path from "path";
+export function server(env: any): Configuration {
+    const isDevBuild: boolean = !(env && env.prod);
+    var plugins: Plugin[] = [
+        new DllReferencePlugin({
+            context: __dirname,
+            manifest: require(helper.root("wwwroot", "dist", "vendor-manifest.json")),
+            sourceType: "commonjs2",
+            name: "./vendor"
+        })
+    ];
+    if (isDevBuild) {
+        plugins.push(
+            // plugins that apply in development builds only
+            new AngularCompilerPlugin({
+                tsConfigPath: "./tsconfig.json",
+                entryModule: helper.root("client", "app", "app.server.module#AppModule")
+            })
+        );
+    }
+    return merge(common(env), {
+        resolve: {
+            mainFields: ["main"]
+        },
+        entry: {
+            "server": helper.root("client", "boot.server.ts")
+        },
+        plugins: plugins,
+        output: {
+            libraryTarget: "commonjs",
+            path: helper.root("client", "dist")
+        },
+        target: "node",
+        devtool: "inline-source-map"
+    });
+}
