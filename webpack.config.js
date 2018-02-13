eac5b7658f061e475819b0e355002d306d262be5
Rasmus <rsj-kc@outlook.com>
2018-02-13 13:57:09 +0100

    .gitignore is now working

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var webpack_server_1 = require("./config/webpack.server");
-var webpack_browser_1 = require("./config/webpack.browser");
-module.exports = function (env) {
-    return [webpack_browser_1.browser(env), webpack_server_1.server(env)];
-};
--- webpack.vendor.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var webpack_vendor_server_1 = require("./config/webpack.vendor.server");
-var webpack_vendor_browser_1 = require("./config/webpack.vendor.browser");
-module.exports = function (env) {
-    return [webpack_vendor_browser_1.browser(env), webpack_vendor_server_1.server(env)];
-};
