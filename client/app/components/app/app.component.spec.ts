e64b866052606d7d268515c211471c97902ceaad
Rasmus Sloth Jensen <rasmus@digitaltunaslad.com>
2018-03-29 19:04:11 +0200

    added unit testing

---
+++ client/app/components/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { RouterTestingModule } from "@angular/router/testing";
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+describe("AppComponent", () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule
+            ],
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
+    }));
+    it("should create the app", async(() => {
+        const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+        const app: AppComponent = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
+    it(`should have as title 'App'`, async(() => {
+        const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+        const app: AppComponent = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual("App");
+    }));
+    it("should render copyright information in the footer", async(() => {
+        const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled: HTMLElement = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector("h1").textContent).toContain("AppComponent");
+    }));
+});
