35d657de2f170b9f4f9addd22f29fb92430ff8ba
Rasmus <rsj-kc@outlook.com>
2018-04-06 08:03:50 +0200

    added testing

--- client/app/app.shared.module.ts
+++ client/app/app.shared.module.ts
@@ -1,12 +1,15 @@
 import "../theming/theme.scss";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule, BrowserTransferStateModule } from "@angular/platform-browser";
 import { TransferHttpCacheModule } from "@nguniversal/common";
 import { AppComponent, HomeComponent, AboutComponent } from "./components";
 import { AppRouteModule } from "./app.route.module";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { ORIGIN_URL } from "@nguniversal/aspnetcore-engine/tokens";
+import { TranslateLoaderFactory } from "./translate-loader.factory";
 import { MatCardModule } from "@angular/material";
 @NgModule({
     imports: [
@@ -19,12 +22,22 @@ import { MatCardModule } from "@angular/material";
         BrowserTransferStateModule,
         FormsModule,
         AppRouteModule,
-        MatCardModule
+        MatCardModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: TranslateLoaderFactory,
+                deps: [HttpClient, [ORIGIN_URL]]
+            }
+        })
     ],
     declarations: [
         AppComponent,
         HomeComponent,
         AboutComponent
+    ],
+    providers: [
+        TranslateModule
     ]
 })
 export class AppSharedModule { }
--- client/app/components/app/app.component.ts
+++ client/app/components/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewEncapsulation } from "@angular/core";
-
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { ViewEncapsulation } from "@angular/core";
 @Component({
   selector: "app",
   templateUrl: "app.component.html",
@@ -8,4 +9,11 @@ import { Component, ViewEncapsulation } from "@angular/core";
 })
 export class AppComponent {
     public title: string = "App";
+    constructor(public translate: TranslateService) {
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang("en");
+
+        // the lang to use, if the lang isn't available, it will use the current loader to get them
+        translate.use("en");
+    }
 }
