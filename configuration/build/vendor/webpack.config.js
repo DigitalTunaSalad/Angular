1e3845a94f80ec91a46d84017dce14ac04956ce7
Rasmus Sloth Jensen <rasmus@digitaltunaslad.com>
2018-04-11 21:41:58 +0200

    gitignore now working

--- configuration/build/vendor/webpack.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var client = require("./client.config");
-var server = require("./server.config");
-module.exports = function (env) {
-    return [client.configure(env), server.configure(env)];
-};
